bc29c773df3f53c73fdd8ba8eb27344b25e8b47f
Destin <dvhunt7@gmail.com>
2018-03-13 22:49:12 -0400

    more changes

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 //express, body parser, path
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.post("/api/new", function (req, res) {
+    // req.body is equal to the JSON data sent with the post request
+    // This is what the body-parser middleware does for us
+    //   console.log(req.body);
+        reservation.tables.push(req.body);
+        res.json(req.body);
+        console.log(reservation.tables);
+        console.log(reservation.tables.length);
+        return true;
+
+});
+
+module.exports = app;
